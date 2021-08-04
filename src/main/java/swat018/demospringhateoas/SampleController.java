038ec1e40d2e222335bef827cdbe91a5b86b5ee7
jinwoo park <swat018@gmail.com>
2021-08-05 06:19:42 +0900

    스프링 웹  MVC 10부: Spring HATEOAS

---
+++ src/main/java/swat018/demospringhateoas/SampleController.java
@@ -0,0 +1,24 @@
+package swat018.demospringhateoas;
+
+import org.springframework.hateoas.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/hello")
+    public Resource<Hello> hello() {
+        Hello hello = new Hello();
+        hello.setPrefix("Hey,");
+        hello.setName("jinwoo");
+
+        Resource<Hello> helloResource = new Resource<>(hello);
+        helloResource.add(linkTo(methodOn(SampleController.class).hello()).withSelfRel());
+
+        return helloResource;
+    }
+}
--- src/test/java/swat018/demospringhateoas/DemospringhateoasApplicationTests.java
+++ src/test/java/swat018/demospringhateoas/DemospringhateoasApplicationTests.java
@@ -1,6 +1,6 @@
 package swat018.demospringhateoas;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
