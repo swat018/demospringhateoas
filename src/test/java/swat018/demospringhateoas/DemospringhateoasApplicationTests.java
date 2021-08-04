1945ebb0b96a3f6dca6debeb2b9d70770de61b21
jinwoo park <swat018@icloud.com>
2021-08-04 21:34:57 +0900

    Initial commit

---
+++ src/test/java/swat018/demospringhateoas/DemospringhateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package swat018.demospringhateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringhateoasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
